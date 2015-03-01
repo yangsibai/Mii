aaaa32569821e28f5a4bbf09854a7cf1fb483c45
yangsibai <yangsibai@gmail.com>
2015-03-01 17:27:34 +0800

    first commit

---
+++ Mii/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mii
+//
+//  Created by yangsibai on 15/3/1.
+//  Copyright (c) 2015年 massimo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mii/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mii
+//
+//  Created by yangsibai on 15/3/1.
+//  Copyright (c) 2015年 massimo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
