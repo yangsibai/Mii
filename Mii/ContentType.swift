de216943c83a5c5fc4e024bf16c4416e6377c44c
yangsibai <yangsibai@gmail.com>
2015-03-07 19:39:47 +0800

    merge minify service

---
+++ Mii/ContentType.swift
@@ -0,0 +1,15 @@
+//
+//  ContentType.swift
+//  Mii
+//
+//  Created by yangsibai on 15/3/7.
+//  Copyright (c) 2015年 massimo. All rights reserved.
+//
+
+import Foundation
+
+enum ContentType {
+    case CSS
+    case JavaScript
+    case HTML
+}
--- Mii/MinifierService.swift
+++ Mii/MinifierService.swift
@@ -10,6 +10,31 @@ import Foundation
 import SwiftHTTP
 
 class MinifierService{
+    
+    func minify(type:ContentType, content:String, success:(String) -> Void, failure: (NSError) -> Void) -> Void{
+        var apiURL:String = ""
+        switch(type){
+            case ContentType.CSS:
+                apiURL = "http://cssminifier.com/raw"
+        case ContentType.JavaScript:
+            apiURL = "http://javascript-minifier.com/raw"
+        default:
+            failure(NSError(domain: "unknown content type", code: -1, userInfo: nil))
+        }
+        var request = HTTPTask()
+        let params: Dictionary<String, AnyObject> = ["input": content]
+        request.POST(apiURL, parameters: params, success: {(response: HTTPResponse) in
+            if let data = response.responseObject as? NSData{
+                let str = NSString(data: data, encoding: NSUTF8StringEncoding)
+                return success(str!)
+            }
+            success("")
+            },failure: {(error: NSError, response: HTTPResponse?) in
+                failure(error)
+        })
+    }
+    
+    
     func minifyCss(css:String, success:(String) -> Void, failure: (NSError) -> Void)-> Void{
         var request = HTTPTask()
         let params: Dictionary<String, AnyObject> = ["input": css]
